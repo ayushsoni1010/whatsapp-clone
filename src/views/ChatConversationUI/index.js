9674feffc3fc92e88b72389d8c352368300033ac
Ayush Soni <mr.soni1002001@gmail.com>
2021-10-07 23:32:07 +0530

    🚀Update: Header and Footer

--- src/views/ChatConversationUI/index.js
+++ src/views/ChatConversationUI/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ConversationFooter from "../../components/ConversationFooter";
+import ConversationFooter from "../../components/section-componets/ConversationFooter";
 import ConversationHeader from "../../components/section-componets/ConversationHeader";
 import "./style.views.chatConversationUI.css";
 
 export default function ChatConversationUI() {
     return (
-      <div className="portrait_view" style={{ "margin-left": "28.824em" }}>
+      <div className="portrait_view" style={{ "margin-left": "30%" }}>
         <ConversationHeader
           profile="https://avatars.githubusercontent.com/u/76895975"
           name="Ayush Soni"
